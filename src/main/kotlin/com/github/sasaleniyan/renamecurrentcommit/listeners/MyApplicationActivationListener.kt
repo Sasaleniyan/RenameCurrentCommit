3a5aca5036de11d5d098d33c3cee52a436349659
GitHub Action <action@github.com>
2025-03-18 14:47:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/sasaleniyan/renamecurrentcommit/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sasaleniyan/renamecurrentcommit/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sasaleniyan.renamecurrentcommit.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
